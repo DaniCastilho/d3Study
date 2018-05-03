5d2ed8d12262c33f6b712c528c2d0c346205c889
Dani Castilho <danicastilhodev@gmail.com>
2018-05-03 00:17:08 -0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# d3Study
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,35 @@
+import * as d3 from "d3";
+import _ from "lodash";
+
+const dataset = _.map(_.range(30), i => Math.random() * 500);
+const w = 400;
+const h = 300;
+
+const svg = d3
+  .select("#chartArea")
+  .append("svg")
+  .attr("width", w)
+  .attr("height", h);
+
+var xScale = d3
+  .scaleBand()
+  .domain(dataset)
+  .paddingInner(0.1)
+  .paddingOuter(0)
+  .rangeRound([0, w]);
+
+var yScale = d3
+  .scaleLinear()
+  .domain([0, d3.max(dataset)])
+  .range([0, h]);
+
+svg
+  .selectAll("rect")
+  .data(dataset)
+  .enter()
+  .append("rect")
+  .attr("class", "bar")
+  .attr("x", d => xScale(d))
+  .attr("y", d => h - yScale(d))
+  .attr("width", xScale.bandwidth())
+  .style("height", d => yScale(d));
