fb360f2eeb1eedb757a3cbd2bb97ccb4a51d9e88
Dani Castilho <danicastilhodev@gmail.com>
2018-05-03 20:59:00 -0300

    trying quantile

--- src/index.js
+++ src/index.js
@@ -24,14 +24,20 @@ import _ from "lodash";
     .domain([0, d3.max(dataset)])
     .range([0, h]);
 
+  var colorScale = d3
+    .scaleQuantile()
+    .domain([0, 10, dataset.length - 15])
+    .range(["blue", "purple", "pink", "green"]);
+
   svg
     .selectAll("rect")
     .data(dataset)
     .enter()
     .append("rect")
     .attr("class", "bar")
-    .attr("x", d => xScale(d))
+    .attr("x", xScale)
     .attr("y", d => h - yScale(d))
     .attr("width", xScale.bandwidth())
-    .style("height", d => yScale(d));
+    .style("height", yScale)
+    .attr("fill", (d, i) => colorScale(i));
 })();
