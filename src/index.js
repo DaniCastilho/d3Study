726c66c48683764dd38bd539f789a406724fc01c
Dani Castilho <danicastilhodev@gmail.com>
2018-05-03 22:58:41 -0300

    trying scatter plot

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import * as d3 from "d3";
 import _ from "lodash";
 
 (function() {
-  const dataset = _.map(_.range(30), i => Math.random() * 500);
-  const margin = { top: 20, right: 0, bottom: 30, left: 30 };
+  const dataset = _.map(_.range(25), i => {
+    return {
+      x: Math.random() * 100,
+      y: Math.random() * 100,
+      r: Math.random() * 30
+    };
+  });
+  var margin = { top: 0, right: 0, bottom: 0, left: 0 };
+
   const w = 400 - margin.left - margin.right;
   const h = 300 - margin.top - margin.bottom;
 
@@ -13,34 +20,26 @@ import _ from "lodash";
     .attr("width", w + margin.left + margin.right)
     .attr("height", h + margin.top + margin.bottom)
     .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 
   var xScale = d3
-    .scaleBand()
-    .domain(dataset)
-    .paddingInner(0.1)
-    .paddingOuter(0)
+    .scaleLinear()
+    .domain([0, 100])
     .rangeRound([0, w]);
 
   var yScale = d3
     .scaleLinear()
-    .domain([0, d3.max(dataset)])
-    .range([0, h]);
-
-  var colorScale = d3
-    .scaleQuantile()
-    .domain([0, 10, dataset.length - 15])
-    .range(["blue", "purple", "pink", "green"]);
+    .domain([0, d3.max(dataset, d => d.y)])
+    .range([h, 0]);
 
   svg
-    .selectAll("rect")
+    .selectAll("circle")
     .data(dataset)
     .enter()
-    .append("rect")
-    .attr("class", "bar")
-    .attr("x", xScale)
-    .attr("y", d => h - yScale(d))
-    .attr("width", xScale.bandwidth())
-    .style("height", yScale)
-    .attr("fill", (d, i) => colorScale(i));
+    .append("circle")
+    .attr("class", "bubble")
+    .attr("fill", "purple")
+    .attr("cx", d => xScale(d.x))
+    .attr("cy", d => yScale(d.y))
+    .attr("r", d => d.r);
 })();
