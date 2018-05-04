78ed25f5598d060cbfaf768ca595fbc867790a03
Dani Castilho <danicastilhodev@gmail.com>
2018-05-03 21:07:33 -0300

    tryng margins

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import _ from "lodash";
 
 (function() {
   const dataset = _.map(_.range(30), i => Math.random() * 500);
-  const w = 400;
-  const h = 300;
+  const margin = { top: 20, right: 0, bottom: 30, left: 30 };
+  const w = 400 - margin.left - margin.right;
+  const h = 300 - margin.top - margin.bottom;
 
   const svg = d3
     .select("#chartArea")
     .append("svg")
-    .attr("width", w)
-    .attr("height", h);
+    .attr("width", w + margin.left + margin.right)
+    .attr("height", h + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   var xScale = d3
     .scaleBand()
